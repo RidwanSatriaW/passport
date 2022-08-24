76f8fa6152690515bbcc79dd870a0df86665b85a
Ridwan Satria Wicaksana <wicaksana.rs@gmail.com>
2022-08-24 11:40:47 +0700

    first commit

---
+++ app/Http/Controllers/ApiLoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ApiLoginController extends Controller
+{
+    public function login(Request $request){
+        if(Auth::attempt([
+            'email' => $request->email, 
+            'password' => $request->password
+            ]
+            )){
+            $user = Auth::user();
+            $token = $user->createToken('user')->accessToken;
+            $data['user'] = $user;
+            $data['token'] = $token;
+            return response()->json(['success' => true, 'data' => $data, 'pesan' => 'login berhasil'], 200);
+        }else{
+            return response()->json(['success' => false, 'data' => '', 'pesan' => 'login gagal'], 401);
+        }
+    }
+}
